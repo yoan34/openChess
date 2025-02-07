693398c99a9a78f48d722d3fc48ad90ec5529ae1
yoan <yoan.bousquet@jokosun.com>
2025-02-08 00:47:56 +0100

    update react

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,5 @@
-import { Sidebar } from '@/components/layouts/sidebar'
 import React from 'react'
+import { Sidebar } from '@/components/layouts/sidebar'
 
 export default function AuthLayout({
   children,
---
+++ src/components/layouts/sidebar.tsx
@@ -0,0 +1,57 @@
+import Image from 'next/image'
+import React from 'react'
+import Link from 'next/link'
+import BookSVG from 'public/book.svg'
+import OpeningSVG from 'public/opening.svg'
+import TrophySVG from 'public/trophy.svg'
+
+const navItems = [
+  {
+    title: 'PLay',
+    href: '/play',
+    icon: OpeningSVG
+  },,
+  {
+    title: 'Opening',
+    href: '/opening',
+    icon: OpeningSVG
+  },
+  {
+    title: 'Trophies',
+    href: '/trophy',
+    icon: TrophySVG
+  },
+  {
+    title: 'Library',
+    href: '/library',
+    icon: BookSVG
+  }
+]
+
+export function Sidebar () {
+
+  return (
+    <div className="w-56 h-screen bg-grey-900">
+      <p className="text-2xl font-bold text-white text-center py-4">OpenChess</p>
+      <nav className="p-2">
+        {navItems.map((item) => {
+          return (
+            <Link
+              key={item.href}
+              href={item.href}
+              className="flex items-center gap-3 p-2 hover:bg-grey-700 rounded-lg group">
+              <Image
+                src={item.icon}
+                aria-hidden="true"
+                alt="navIcon"
+                width={32}
+                height={32}
+              />
+              <span className='text-lg text-grey-100 font-bold group-hover:text-white'>{item.title}</span>
+            </Link>
+          )
+        })}
+      </nav>
+    </div>
+  )
+}
